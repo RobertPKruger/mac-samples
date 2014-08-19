c78fbc847194070fb18879312c62ca698c730611
Vincent Dondain <vincent.dondain@xamarin.com>
2014-08-19 12:16:46 -0400

    [CAB - TimedAnimation] - Migrated to 64 bit
    - Sample was updated to 64 bit.

--- CoreAnimationBook/TimedAnimation/MainWindow.xib.designer.cs
+++ CoreAnimationBook/TimedAnimation/MainWindow.xib.designer.cs
@@ -11,18 +11,18 @@
 namespace TimedAnimation {
 	
 	
-	// Should subclass MonoMac.AppKit.NSWindow
-	[MonoMac.Foundation.Register("MainWindow")]
+	// Should subclass AppKit.NSWindow
+	[Foundation.Register("MainWindow")]
 	public partial class MainWindow {
 	}
 	
-	// Should subclass MonoMac.AppKit.NSWindowController
-	[MonoMac.Foundation.Register("MainWindowController")]
+	// Should subclass AppKit.NSWindowController
+	[Foundation.Register("MainWindowController")]
 	public partial class MainWindowController {
 	}
 	
-	// Should subclass MonoMac.AppKit.NSView
-	[MonoMac.Foundation.Register("TimedAnimation")]
+	// Should subclass AppKit.NSView
+	[Foundation.Register("TimedAnimation")]
 	public partial class TimedAnimation {
 	}
 }
