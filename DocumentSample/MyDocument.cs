3b810b5abbf034a79f9988b93e051341af8337e3
Bob Kruger <rpk@electricstory.com>
2014-11-30 16:13:34 -0800

    DocumentSample unified
    Upgraded to unified. Constructor in MyDocument.cs is still broken and
    needs to be fixed.

--- DocumentSample/MyDocument.cs
+++ DocumentSample/MyDocument.cs
@@ -2,8 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using MonoMac.Foundation;
-using MonoMac.AppKit;
+using Foundation;
+using AppKit;
 
 namespace DocumentSample
 {
@@ -16,10 +16,10 @@ namespace DocumentSample
 		}
 
 		// Called when created directly from a XIB file
-		[Export("initWithCoder:")]
-		public MyDocument (NSCoder coder) : base(coder)
-		{
-		}
+		//[Export("initWithCoder:")]
+		//public MyDocument(NSCoder coder) : base(coder)
+		//{
+		//}
 		
 		// Override returning the nib file name of the document
 		// If you need to use a subclass of NSWindowController or if your document supports
